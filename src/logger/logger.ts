587f3afaf3ff4a00a89f174088b0bdcad3cc2a04
erlannn777 <erlank123456789@gmail.com>
2023-03-19 21:51:12 +0600

    LoggerService added

---
+++ src/logger/logger.ts
@@ -0,0 +1,22 @@
+import { Logger } from "tslog";
+import { ILogger } from "./logger.interface";
+
+export class LoggerService implements ILogger {
+  public logger: Logger;
+
+  constructor() {
+    this.logger = new Logger();
+  }
+
+  public log(...args: unknown[]) {
+    this.logger.info(...args);
+  }
+
+  public warn(...args: unknown[]) {
+    this.logger.warn(...args);
+  }
+
+  public error(...args: unknown[]) {
+    this.logger.error(...args);
+  }
+}

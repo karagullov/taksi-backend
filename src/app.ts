885d575188a69e0a7cf2c6d56922607dd47c4d0b
erlannn777 <erlank123456789@gmail.com>
2023-03-19 22:57:09 +0600

    Logger & inversify added

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express, { Express } from "express";
 import { Server } from "http";
+import { inject, injectable } from "inversify";
+import { ILogger } from "./logger/logger.interface";
+import { TYPES } from "./types";
 import { usersRouter } from "./users/users";
 
+@injectable()
 export class App {
   app: Express;
   server: Server;
   port: number;
 
-  constructor() {
+  constructor(@inject(TYPES.ILogger) private logger: ILogger) {
     this.app = express();
     this.port = 8000;
   }
@@ -19,6 +23,6 @@ export class App {
   public async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port);
-    console.log(`Сервер запущен на http://localhost:${this.port}`);
+    this.logger.log(`Сервер запущен на http://localhost:${this.port}`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import "reflect-metadata";
+import { Container, ContainerModule, interfaces } from "inversify";
 import { App } from "./app";
+import { LoggerService } from "./logger/logger";
+import { ILogger } from "./logger/logger.interface";
+import { TYPES } from "./types";
 
-async function bootstrap() {
-  const app = new App();
-  await app.init();
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+  appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService);
+  appContainer.bind<App>(TYPES.Application).to(App);
+});
+
+function bootstrap() {
+  const appContainer = new Container();
+  appContainer.load(appBindings);
+
+  const app = appContainer.get<App>(TYPES.Application);
+  app.init();
+
+  return { app, appContainer };
 }
 
-bootstrap();
+export const { app, appContainer } = bootstrap();
